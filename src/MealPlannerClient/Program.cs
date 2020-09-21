46e74c9e566c2cb1ed799c5c4dedb06c29d8fb61
Umair Hussein <umahus@melinmedical.com>
2020-09-21 23:05:20 +0200

    project setup

---
+++ src/MealPlannerClient/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+namespace MealPlannerClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSingleton(services =>
+            {
+                // Get the service address from appsettings.json
+                var config = services.GetRequiredService<IConfiguration>();
+                var backendUrl = config["BackendUrl"];
+
+                // Create a channel with a GrpcWebHandler that is addressed to the backend server.
+                //
+                // GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
+                // then GrpcWeb is recommended because it produces smaller messages.
+                var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
+
+                return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions { HttpHandler = httpHandler });
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src/MealPlannerServer/Services/MealPlannerService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace MealPlannerServer.Services
+{
+    public class MealPlannerService : MealPlanner.MealPlannerBase
+    {
+        private readonly ILogger<MealPlannerService> _logger;
+
+        public MealPlannerService(ILogger<MealPlannerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<MealRegisterResponse> RegisterMeal(MealRegisterRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"Data received {request.MealName}  {request.MealDateTime}");
+
+            return new MealRegisterResponse
+            {
+                Response = $"Registered sucessfully. Data {request.MealName}  {request.MealDateTime}"
+            };
+        }
+
+        public override async Task GetRegisteredMeals(GetAllMealsRequest request, IServerStreamWriter<MealRegisterResponse> responseStream, ServerCallContext context)
+        {
+            var client = new HttpClient();
+
+            var data = await client.GetAsync("https://jsonplaceholder.typicode.com/comments");
+
+            if (data.IsSuccessStatusCode)
+            {
+                string responseBody = await data.Content.ReadAsStringAsync();
+
+                var resp = JsonConvert.DeserializeObject<List<Comment>>(responseBody);
+                
+                _logger.LogInformation($"Received comments data from url Total {resp.Count}");
+
+                foreach (var comment in resp)
+                {
+                    if (context.CancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+
+                    await Task.Delay(100);
+
+                    await responseStream.WriteAsync(new MealRegisterResponse { Response = comment.ToString() });
+                }
+
+            }
+        }
+    }
+
+    public class Comment
+    {
+        public int postId { get; set; }
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public string body { get; set; }
+
+        public override string ToString()
+        {
+           return $"PostId : {postId} Id : {id} Name : {name} Email : {email}";
+        }
+    }
+}
